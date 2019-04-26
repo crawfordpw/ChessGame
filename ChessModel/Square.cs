11b2a46df04562a8d05664ce15b41543d322ed67
Patrick Crawford <pwc.crawford@gmail.com>
2019-04-26 01:58:40 -0400

    Movement via button clicking has been imlemented. displays properly in the UI but I suspect there may be a better way to do it. (not having to clear and re-add all squares to ItemsSource)

--- ChessModel/Square.cs
+++ ChessModel/Square.cs
@@ -1,11 +1,14 @@
-﻿namespace ChessModel
+﻿using System.ComponentModel;
+
+namespace ChessModel
 {
-    public class Square
+    public class Square : INotifyPropertyChanged
     {
         public int ColID { get; set; }
         public int RowID { get; set; }
         public ChessColor Color { get; set; }
         public IPiece Piece { get; set; }
+        public string Cord { get; set; }
 
         public Square() : this(0, 0)
         {
@@ -16,12 +19,21 @@
         {
             RowID = row;
             ColID = col;
+            Cord = $"{row}{col}";
         }
 
         public void MakeSameCord()
         {
             Piece.ColID = ColID;
             Piece.RowID = RowID;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
         }
     }
 }
--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -22,18 +22,25 @@ namespace UI
     /// </summary>
     public partial class MainWindow : Window
     {
-        public ObservableCollection<Square> ChessBoard { get; set; }
+        ObservableCollection<Square> ChessBoard { get; set; }
+        private Game game { get; set; }
+        public Square FromSquare { get; set; }
+        public Square ToSquare { get; set; }
 
         public MainWindow()
         {
-            this.DataContext = this;
+            DataContext = this;
             InitializeComponent();
 
-            Game game = new Game();
+            FromSquare = null;
+            ToSquare = null;
+            game = new Game();
             game.NewGame();
 
-            this.ChessBoard = new ObservableCollection<Square>();
+            ChessBoard = new ObservableCollection<Square>();
             ConvertToList(game, ChessBoard);
+            //game.gl.MovePiece(game.gb.squares[1, 0], game.gb.squares[2, 0]);
+            //game.gl.MovePiece(game.gb.squares[2, 0], game.gb.squares[3, 0]);
         }
 
         public void ConvertToList(Game game, ObservableCollection<Square> ChessBoard)
@@ -46,6 +53,41 @@ namespace UI
                 }
             }
             Board.ItemsSource = ChessBoard;
+        }
+
+        public void HandleGame(object sender, RoutedEventArgs e)
+        {
+            HandleMovement(sender);
+        }
+
+        private void HandleMovement(object sender)
+        {
+            var button = (Button)sender;
+            var tag = button.Tag.ToString();
+            int row = (int)Char.GetNumericValue(tag[0]);
+            int col = (int)Char.GetNumericValue(tag[1]);
+
+            if (FromSquare == null)
+            {
+                FromSquare = game.gb.squares[row, col];
+                if (game.gb.squares[row, col].Piece == null)
+                {
+                    FromSquare = null;
+                }
+            }
+            else
+            {
+                ToSquare = game.gb.squares[row, col];
+            }
+
+            if (FromSquare != null && ToSquare != null)
+            {
+                game.gl.MovePiece(FromSquare, ToSquare);
+                FromSquare = null;
+                ToSquare = null;
+                ChessBoard.Clear();
+                ConvertToList(game, ChessBoard);
+            }           
         }
     }
 }
