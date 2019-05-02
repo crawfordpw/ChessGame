51e9298abe8902a2e70b704548b88e552be57cb1
Patrick Crawford <pwc.crawford@gmail.com>
2019-05-02 15:05:17 -0400

    Reorganized UI. Started to re-style end game window

--- UI/Windows/EndGameWindow.xaml.cs
+++ UI/Windows/EndGameWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using ChessModel;
 
 namespace UI
 {
@@ -21,6 +10,7 @@ namespace UI
     {
         public string Selection { get; set; }
         public string Message { get; set; }
+        public Player Player { get; set; }
 
         public EndGameWindow() : this("Checkmate! White Wins!")
         {
@@ -31,8 +21,11 @@ namespace UI
         public EndGameWindow(string message)
         {
             DataContext = this;
+            Game Game = new Game();
+            Player = Game.Player1;
+
             InitializeComponent();
-            message = $"{message}\n\n\nPress \"New Game\" to start a new game. Press \"End Game\" to close the Application";
+            message = $"{message}";
             this.Message = message;
         }
 
